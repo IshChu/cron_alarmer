0357479d179cba8fb0312a07dbcf43fc68b79709
Ihtesham Chowdhury <ninjalabib@gmail.com>
2021-02-04 10:43:07 -0500

    forgot to push the 14,44 times

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const sound = require("sound-play");
 const path = require("path");
 const filePath = path.join(__dirname, "alarm.mp3");
 
-let job = new CronJob('*/5 * * * * *', () => {
+let job = new CronJob('0 14,44 * * * *', () => {
   console.log('Every 5 Second');
   sound.play(filePath);
 }, null, true, 'America/New_York');
