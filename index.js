e6014b5359c786de14949b20dd16700298c355a5
Ihtesham Chowdhury <ninjalabib@gmail.com>
2021-02-04 20:10:55 -0500

    Adding a notifier as well

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
-var CronJob = require('cron').CronJob;
+const argv = require('minimist')(process.argv.slice(2), {
+  default: { cron: '0 14,44 * * * *' }
+});
+console.log(`Using cron string of ${argv.cron}`);
+
+const CronJob = require('cron').CronJob;
 const sound = require("sound-play");
+const notifier = require('node-notifier');
+
 const path = require("path");
 const filePath = path.join(__dirname, "alarm.mp3");
 
-let job = new CronJob('0 14,44 * * * *', () => {
-  console.log('Every 5 Second');
+let job = new CronJob(argv.cron, () => {
+  notifier.notify({
+	  title: 'Cron Alarm Notification',
+	  message:'It\'s TIME',
+	  icon: path.join(__dirname, 'toast.png'),
+	  sound: false,
+	  wait: false
+	  });
   sound.play(filePath);
 }, null, true, 'America/New_York');
 
